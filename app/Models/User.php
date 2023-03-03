5c1e775b7b14c67935cbf83fb16f2f40578eb3c6
Tenesh Vignesan <teneshdev@gmail.com>
2023-03-03 12:26:12 +0800

    build:  Init project

---
+++ app/Models/User.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Models;
+
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Symfony\Component\HttpFoundation\Session\Attribute\AttributeBag;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'firstname',
+        'lastname',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'firstname' => 'string',
+        'lastname' => 'string',
+        'email' => 'string',
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * Interact with the user's password.
+     *
+     * @return Attribute
+     */
+    protected function password(): Attribute
+    {
+        return Attribute::make(
+            set: fn ($value) => bcrypt($value),
+        );
+    }
+
+    /**
+     * Interact with the user's first name.
+     *
+     * @return Attribute
+     */
+    protected function firstname(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => ucfirst($value),
+            set: fn ($value) => strtolower($value),
+        );
+    }
+
+    /**
+     * Interact with the user's last name.
+     *
+     * @return Attribute
+     */
+    protected function lastname(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => ucfirst($value),
+            set: fn ($value) => strtolower($value),
+        );
+    }
+}
