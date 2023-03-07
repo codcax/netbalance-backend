dd871ec0546a9023fa72ab0ac73d70d7e3decbc2
Tenesh Vignesan <teneshdev@gmail.com>
2023-03-07 18:02:07 +0800

    feature: Update login request

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -37,6 +37,7 @@ class LoginRequest extends FormRequest
                     ->numbers()
                     ->symbols()
             ],
+            'remember' => ['required', 'boolean']
         ];
     }
 
