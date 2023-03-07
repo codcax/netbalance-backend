2dd10e4b2c7cb074c188a21e11bbf2f2446661eb
Tenesh Vignesan <teneshdev@gmail.com>
2023-03-07 17:27:05 +0800

    feature: Add user related features

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\User\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum', 'verified')->group(function () {
+    Route::resources(['users' => UserController::class], ['except' => ['create', 'edit']]);
 });
