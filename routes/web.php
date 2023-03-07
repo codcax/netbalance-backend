194c09410d8b4fe9b1e4c7283649c258e3c6427b
Tenesh Vignesan <teneshdev@gmail.com>
2023-03-07 16:46:56 +0800

    build: Scaffold breeze package auth

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +14,7 @@ use App\Http\Controllers\AuthController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return ['Laravel' => app()->version()];
 });
 
-Route::post('signup', [AuthController::class, 'signup']);
-Route::post('signin', [AuthController::class, 'signin']);
+require __DIR__.'/auth.php';
